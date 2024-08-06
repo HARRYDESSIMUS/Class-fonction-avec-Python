6631d8c87438ed1be9912b734e02154682a9c3cd
HARRYDESSIMUS <95467760+HARRYDESSIMUS@users.noreply.github.com>
2024-08-06 13:01:45 +0200

    Add files via upload

--- compte_bancaire.py
+++ compte_bancaire.py
@@ -7,7 +7,22 @@ class CompteBancaire:
     def deposer(self,montant):
         self.solde += montant
         print(f"{montant}€ deposé . Nouveau solde : {self.solde}€")
+
+
     def retirer(self,montant):
         if montant<=self.solde:
             self.solde -= montant
-            print(f"{montant} €")
+            print(f"{montant} € retirés.nouveau solde:{self.solde} €")
+        else: 
+            print(f"Fond insuffisant")
+    
+    def afficher_solde(self):
+        print(f"solde de {self.titulaire}:{self.solde}")
+
+compte1= CompteBancaire("Jean")
+compte1.afficher_solde()
+compte1.deposer(100)
+compte1.retirer(50)
+compte1.retirer(100)    
+
+
